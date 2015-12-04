27011737a9baa5fa17911d193c875bcb643a7d62
Tibor Tálosi <teeebor@gmail.com>
2015-12-04 10:40:09 +0100

    - Excel export sample - YAML config backslash characters escaped

--- src/Calendar/Controller/CalendarController.php
+++ src/Calendar/Controller/CalendarController.php
@@ -44,4 +44,47 @@ class CalendarController extends ActionController
 		return $view;
 	}
 
+	public function save(){
+		$this->setTemplate('application/text');
+		$this->getApplication()->getRequestHeader()->setVariable('Content-Type','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		$this->getApplication()->getRequestHeader()->setVariable('Content-Disposition','attachment;filename="01simple.xlsx"');
+		$this->getApplication()->getRequestHeader()->setVariable('Cache-Control','max-age=0');
+
+		$this->getApplication()->getRequestHeader()->setHeaders();
+
+		$objPHPExcel = new \PHPExcel();
+		$objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
+				->setLastModifiedBy("Maarten Balliauw")
+				->setTitle("Office 2007 XLSX Test Document")
+				->setSubject("Office 2007 XLSX Test Document")
+				->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+				->setKeywords("office 2007 openxml php")
+				->setCategory("Test result file");
+
+
+// Add some data
+		$objPHPExcel->setActiveSheetIndex(0)
+				->setCellValue('A1', 'Hello')
+				->setCellValue('B2', 'world!')
+				->setCellValue('C1', 'Hello')
+				->setCellValue('D2', 'world!');
+
+// Miscellaneous glyphs, UTF-8
+		$objPHPExcel->setActiveSheetIndex(0)
+				->setCellValue('A4', 'Miscellaneous glyphs')
+				->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
+
+// Rename worksheet
+		$objPHPExcel->getActiveSheet()->setTitle('Simple');
+
+
+// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+		$objPHPExcel->setActiveSheetIndex(0);
+
+
+		$objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+		$objWriter->save('php://output');
+		$this->forward()->disableRender();
+		return null;
+	}
 }
